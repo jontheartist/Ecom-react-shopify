a952e253362a015e826b0d8c57141ba3dc45b7f0
Jon B <jonlondontech@gmail.com>
2020-05-09 19:52:35 +0100

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { Provider as StyletronProvider, DebugEngine } from "styletron-react";
 import { Client as Styletron } from "styletron-engine-atomic";
 import {
@@ -10,6 +10,9 @@ import {
 import ShopProvider from '../context/shopContext'
 import HomePage from '../pages/HomePage'
 import ProductPage from '../pages/ProductPage'
+import { Navbar } from '../components/Navbar'
+import { Cart } from '../components/Cart'
+
 
 const debug =
   process.env.NODE_ENV === "production" ? void 0 : new DebugEngine();
@@ -25,6 +28,8 @@ function App() {
     <ShopProvider>
       <StyletronProvider value={engine} debug={debug} debugAfterHydration>
         <Router>
+          <Navbar />
+          <Cart />
           <Switch>
             <Route path="/product/:id">
               <ProductPage />
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/shopContext'
+import { Text, Div, Button, Row, Col, Container } from 'atomize'
 
 export const ProductPage = () => {
+
+    let { id } = useParams();
+    const { fetchProductWithId, addItemToCheckout, product, openCart } = useContext(ShopContext)
+
+    useEffect(() => {
+        fetchProductWithId(id)
+        return () => {
+
+        };
+    }, [fetchProductWithId, id]);
+
+    if (!product.title) return <div> Loading </div>
     return (
-        <div>
-            Product Page
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Div bgImg={product.images[0].src} bgSize="cover" bgPos="center center" h="40rem">
+
+                    </Div>
+                </Col>
+                <Col>
+                    <Text>{product.title}</Text>
+                    <Text>{product.variants[0].price}</Text>
+                    <Button onClick={() => {
+                        addItemToCheckout(product.variants[0].id, 1)
+                        openCart()
+                    }}
+                    > Add To Cart </Button>
+                </Col>
+            </Row>
+        </Container>
+
     )
 }
 
